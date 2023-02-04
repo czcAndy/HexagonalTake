bd6d144b8beeb86930352eaffc8657980c5badb6
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2023-02-04 11:54:00 +0100

    Initial commit

---
+++ src/main/java/com/demo/hexagonal/HexagonalTakeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalTakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalTakeApplication.class, args);
+    }
+
+}
